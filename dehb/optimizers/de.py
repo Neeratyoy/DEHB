b2d3d05faf947077b167457ae84a508e357677a2
neeratyoy <neeratyoy@gmail.com>
2019-10-20 18:32:38 +0200

    Initial class structure for DE

--- dehb/optimizers/de.py
+++ dehb/optimizers/de.py
@@ -1,1 +1,76 @@
-import
+import numpy as np
+import ConfigSpace
+
+
+class DEBase():
+    def __init__(self, b=None, cs=None, dimensions=None, pop_size=None, mutation_factor=None,
+                 crossover_prob=None, strategy='rand1', max_budget=None, **kwargs):
+        self.b = b
+        self.cs = self.b.get_configuration_space() if cs is None else cs
+        self.dimensions = len(self.cs.get_hyperparameters()) if dimensions is None else dimensions
+
+        self.pop_size = pop_size
+        self.mutation_factor = mutation_factor
+        self.crossover_prob = crossover_prob
+        self.strategy = strategy
+
+        self.output_path = kwargs['output_path'] if 'output_path' in kwargs else ''
+
+    def sample_population(self, pop_size=10):
+        return np.random.uniform(low=0.0, high=1.0, size=(pop_size, self.dimensions))
+
+    def boundary_check(self, vector, fix_type='random'):
+        '''
+        Checks whether each of the dimensions of the input vector are within [0, 1].
+        If not, values of those dimensions are replaced with the type of fix selected.
+
+        Parameters
+        ----------
+        vector : array
+            The vector describing the individual from the population
+        fix_type : str, {'random', 'clip'}
+            if 'random', the values are replaced with a random sampling from (0,1)
+            if 'clip', the values are clipped to the closest limit from {0, 1}
+
+        Returns
+        -------
+        array
+        '''
+        violations = np.where((vector > 1) | (vector < 0))[0]
+        if len(violations) == 0:
+            return vector
+        if fix_type == 'random':
+            vector[violations] = np.random.uniform(low=0.0, high=1.0, size=len(violations))
+        else:
+            vector[violations] = np.clip(vector[violations], a_min=0, a_max=1)
+        return vector
+
+    def vector_to_configspace(self, vector):
+        '''Converts numpy array to ConfigSpace object
+
+        Works when self.cs is a ConfigSpace object and the input vector is in the domain [0, 1].
+        '''
+        new_config = self.cs.sample_configuration()
+        for i, hyper in enumerate(self.cs.get_hyperparameters()):
+            if type(hyper) == ConfigSpace.OrdinalHyperparameter:
+                ranges = np.arange(start=0, stop=1, step=1/len(hyper.sequence))
+                param_value = hyper.sequence[np.where((vector[i] < ranges) == False)[0][-1]]
+            elif type(hyper) == ConfigSpace.CategoricalHyperparameter:
+                ranges = np.arange(start=0, stop=1, step=1/len(hyper.choices))
+                param_value = hyper.choices[np.where((vector[i] < ranges) == False)[0][-1]]
+            else:  # handles UniformFloatHyperparameter & UniformIntegerHyperparameter
+                # rescaling continuous values
+                param_value = hyper.lower + (hyper.upper - hyper.lower) * vector[i]
+                if type(hyper) == ConfigSpace.UniformIntegerHyperparameter:
+                    param_value = np.round(param_value).astype(int)   # converting to discrete (int)
+            new_config[hyper.name] = param_value
+        return new_config
+
+    def mutation(self):
+        raise NotImplementedError("The function needs to be defined in the sub class.")
+
+    def crossover(self):
+        raise NotImplementedError("The function needs to be defined in the sub class.")
+
+    def evolve(self):
+        raise NotImplementedError("The function needs to be defined in the sub class.")
