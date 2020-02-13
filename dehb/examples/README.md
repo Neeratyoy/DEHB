8f825ffbb69da2c6fcf9f1340c36ab4ea1b059bc
neeratyoy <de4nas@gmail.com>
2020-02-14 00:28:48 +0100

    Updating to run SVM Surrogate

---
+++ dehb/examples/README.md
@@ -0,0 +1,12 @@
+To install the required HPOlib2 for benchmarks: 
+
+`pip install git+https://github.com/automl/HPOlib2.git@development`
+
+
+To install HpBandSter:
+
+`pip install hpbandster`
+
+
+SciPy needed: 1.2.1
+
--- dehb/examples/svm/svm_surrogate.py
+++ dehb/examples/svm/svm_surrogate.py
@@ -1,4 +1,7 @@
 import os
+import sys
+sys.path.append(os.path.join(os.getcwd(), '../HpBandSter/icml_2018_experiments/experiments'))
+
 import argparse
 
 from workers.svm_surrogate import SVMSurrogateWorker as Worker
@@ -14,23 +17,41 @@ logging.basicConfig(level=logging.ERROR)
 ################################################################################
 
 
-parser = argparse.ArgumentParser(description='Run different optimizers on the CountingOnes problem.', conflict_handler='resolve')
+parser = argparse.ArgumentParser(description='Run different optimizers on the SVM Surrogates '
+                                             'problem.', conflict_handler='resolve')
 parser = util.standard_parser_args(parser)
 
-
 # add benchmark specific arguments
-parser.add_argument('--surrogate_path', type=str, help='Path to the pickled surrogate models. If None, HPOlib2 will automatically download the surrogates to the .hpolib directory in your home directory.', default=None)
-parser.add_argument('--min_budget', type=float, help='Smallest fraction of the full dataset that is used.', default=1/512)
-parser.add_argument('--max_budget', type=float, help='Largest fraction of the full dataset that is used.', default=1)
+parser.add_argument('--dest_dir', type=str, help='the destination directory', default='./results/')
+parser.add_argument('--num_iterations', type=int, default=4,
+                    help='number of Hyperband iterations performed.')
+parser.add_argument('--run_id', type=str, default=0)
+parser.add_argument('--runs', type=int, default=None)
+parser.add_argument('--method', type=str, default='randomsearch',
+                    help='Possible choices: randomsearch, bohb, hyperband, tpe, smac')
+parser.add_argument('--surrogate_path', type=str, default=None,
+                    help='Path to the pickled surrogate models. If None, HPOlib2 will '
+                         'automatically download the surrogates to the .hpolib directory '
+                         'in your home directory.')
+parser.add_argument('--min_budget', type=float, default=1/512,
+                    help='Smallest fraction of the full dataset that is used.')
+parser.add_argument('--max_budget', type=float, default=1,
+                    help='Largest fraction of the full dataset that is used.')
+parser.add_argument('--folder', type=float, default=None,
+                    help='folder to dump output files')
+
 args = parser.parse_args()
 
-#args = parser.parse_args('--method bohb --num_iterations 32 --run_id 2'.split())
+if args.folder is None:
+    folder = args.method
+
+# if args.runs is None:
 
 # this is a synthetic benchmark, so we will use the run_id to separate the independent runs
 worker = Worker(surrogate_path=args.surrogate_path, measure_test_loss=True, run_id=args.run_id)
 
 # directory where the results are stored
-dest_dir = os.path.join(args.dest_dir, "SVMSurrogate")
+dest_dir = os.path.join(args.dest_dir, folder)
 
 # SMAC can be informed whether the objective is deterministic or not
 smac_deterministic = True
@@ -39,3 +60,20 @@ smac_deterministic = True
 result = util.run_experiment(args, worker, dest_dir, smac_deterministic)
 
 print(result.get_incumbent_trajectory())
+
+# else:
+#     for run_id in range(args.runs):
+#         # this is a synthetic benchmark, so we will use the run_id to separate the independent runs
+#         worker = Worker(surrogate_path=args.surrogate_path, measure_test_loss=True, run_id=run_id)
+#
+#         # directory where the results are stored
+#         dest_dir = os.path.join(args.dest_dir, folder)
+#
+#         # SMAC can be informed whether the objective is deterministic or not
+#         smac_deterministic = True
+#
+#         # run experiment
+#         result = util.run_experiment(args, worker, dest_dir, smac_deterministic)
+#
+#         print(result.get_incumbent_trajectory())
+#         worker.shutdown()
