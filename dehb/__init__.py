ecc986ed7906645af165ce0ab5d7475b91b5e696
neeratyoy <neeratyoy@gmail.com>
2020-03-10 02:03:52 +0100

    Adding v4.2.2, v4.2.3 and v7.1

--- dehb/__init__.py
+++ dehb/__init__.py
@@ -2,6 +2,6 @@ from .optimizers import DE
 from .optimizers import DEHBV1, DEHBV1_1
 from .optimizers import DEHBV2
 from .optimizers import DEHBV3, DEHBV3_1, DEHBV3_2
-from .optimizers import DEHBV4, DEHBV4_1, DEHBV4_2
+from .optimizers import DEHBV4, DEHBV4_1, DEHBV4_2, DEHBV4_2_2, DEHBV4_2_3
 from .optimizers import DEHBV6_0, DEHBV6_1, DEHBV6_1_2, DEHBV6_2, DEHBV6_2_2, DEHBV6_3, DEHBV6_3_2
-from .optimizers import DEHBV7_0
+from .optimizers import DEHBV7_0, DEHBV7_1
