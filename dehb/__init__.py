ab3964ffbfca4fb2c729887fd142fd1db09f84c7
neeratyoy <neeratyoy@gmail.com>
2020-03-12 15:51:20 +0100

    gen heuristic exps

--- dehb/__init__.py
+++ dehb/__init__.py
@@ -4,5 +4,5 @@ from .optimizers import DEHBV2
 from .optimizers import DEHBV3, DEHBV3_1, DEHBV3_2
 from .optimizers import DEHBV4, DEHBV4_1, DEHBV4_2, DEHBV4_2_2, DEHBV4_2_3
 from .optimizers import DEHBV6_0, DEHBV6_1, DEHBV6_1_2, DEHBV6_2, DEHBV6_2_2
-from .optimizers import DEHBV6_3, DEHBV6_3_2, DEHBV6_3_3, DEHBV6_3_4
+from .optimizers import DEHBV6_3, DEHBV6_3_2, DEHBV6_3_3, DEHBV6_3_4, DEHBV6_3_5
 from .optimizers import DEHBV7_0, DEHBV7_1
--- dehb/optimizers/__init__.py
+++ dehb/optimizers/__init__.py
@@ -4,5 +4,5 @@ from .dehb import DEHBV2
 from .dehb import DEHBV3, DEHBV3_1, DEHBV3_2
 from .dehb import DEHBV4, DEHBV4_1, DEHBV4_2, DEHBV4_2_2, DEHBV4_2_3
 from .dehb import DEHBV6_0, DEHBV6_1, DEHBV6_1_2, DEHBV6_2, DEHBV6_2_2
-from .dehb import DEHBV6_3, DEHBV6_3_2, DEHBV6_3_3, DEHBV6_3_4
+from .dehb import DEHBV6_3, DEHBV6_3_2, DEHBV6_3_3, DEHBV6_3_4, DEHBV6_3_5
 from .dehb import DEHBV7_0, DEHBV7_1
