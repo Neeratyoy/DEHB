0b141c1a43c796bce9ad2e9704610a4e569a0dc9
neeratyoy <neeratyoy@gmail.com>
2020-04-19 10:47:23 +0200

    Fix hpolib3 bohb

--- dehb/examples/cc18/run_bohb_cc18.py
+++ dehb/examples/cc18/run_bohb_cc18.py
@@ -44,21 +44,6 @@ def convert_to_json(results):
     return res
 
 
-class MyWorker(Worker):
-    def compute(self, config, budget, **kwargs):
-        global n_estimators, max_budget
-        if budget is None:
-            budget = max_budget
-        res = b.objective_function(config, n_estimators=n_estimators, subsample=budget)
-        fitness = res['function_value']
-        cost = res['cost']
-        res = b.objective_function_test(config, n_estimators=n_estimators)
-        return ({
-            'loss': float(fitness),
-            'info': {'cost': float(cost), 'test_loss': float(res['function_value'])}
-        })
-
-
 parser = argparse.ArgumentParser()
 parser.add_argument('--fix_seed', default='False', type=str, choices=['True', 'False'],
                     nargs='?', help='seed')
@@ -103,6 +88,22 @@ n_estimators = args.n_estimators
 min_budget = args.min_budget
 max_budget = args.max_budget
 
+
+class MyWorker(Worker):
+    def compute(self, config, budget, **kwargs):
+        global n_estimators, max_budget
+        if budget is None:
+            budget = max_budget
+        res = b.objective_function(config, n_estimators=n_estimators, subsample=budget)
+        fitness = res['function_value']
+        cost = res['cost']
+        res = b.objective_function_test(config, n_estimators=n_estimators)
+        return ({
+            'loss': float(fitness),
+            'info': {'cost': float(cost), 'test_loss': float(res['function_value'])}
+        })
+
+
 task_ids = get_openmlcc18_taskids()
 if args.task_id not in task_ids:
     raise "Incorrect task ID. Choose from: {}".format(task_ids)
@@ -114,6 +115,7 @@ dimensions = len(cs.get_hyperparameters())
 
 output_path = os.path.join(args.output_path, str(args.task_id), args.folder)
 os.makedirs(output_path, exist_ok=True)
+
 
 runs = args.runs
 for run_id in range(runs):
